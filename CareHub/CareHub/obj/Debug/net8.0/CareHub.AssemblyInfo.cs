566714d5fb01b6cc1952eaff86611b89f8ebf524
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2025-05-07 10:23:55 +0100

    sz

--- CareHub/CareHub/obj/Debug/net8.0/CareHub.AssemblyInfo.cs
+++ CareHub/CareHub/obj/Debug/net8.0/CareHub.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CareHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6c5c03f4a767fcd8e24505742637754f751ae62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40ab0017a331cab2dcf5a9a96e32af83869e465")]
 [assembly: System.Reflection.AssemblyProductAttribute("CareHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CareHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
