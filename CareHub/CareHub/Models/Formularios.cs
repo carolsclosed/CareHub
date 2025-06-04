10cb5007208b1220e781dcea2fba55d31588c534
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2025-06-04 11:32:09 +0100

    atualizar
    atualizar

---
+++ CareHub/CareHub/Models/Formularios.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Sqlite.Query.SqlExpressions.Internal;
+
+
+namespace CareHub.Models;
+
+public class Formularios
+{
+    
+    [Key]  // PK, int, autonumber
+    public int IdForm { get; set; }
+        
+    [ForeignKey(nameof(Utilizador))]
+    public int IdUtil { get; set; }
+      
+    public string nome { get; set; }
+    
+    [RegularExpression("^(?:(?:\\+|00)351)?\\s?(9[1236]\\d{7}|2\\d{8})$\n")]
+    public int telefone { get; set; }
+    
+    [RegularExpression("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$\n")]
+    public string email { get; set; }
+    
+    public string regiao { get; set; }
+    
+    public string descricao { get; set; }
+
+    
+    public Utilizadores Utilizador { get; set; } 
+}
